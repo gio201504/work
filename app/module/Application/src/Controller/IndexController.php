12fc69dadb2020595e3b6d0db30b44c1c9b5f6d3
Gio-PC\Sergio ARNONE <aqwzsxedc50@live.fr>
2016-11-21 21:05:51 +0100

    ajout des sources de cute file browser

--- app/module/Application/src/Controller/IndexController.php
+++ app/module/Application/src/Controller/IndexController.php
@@ -47,4 +47,52 @@ class IndexController extends AbstractActionController
     		return $viewmodel;
     	}
     }
+    
+    public function scanAction()
+    {
+    	$request = $this->getRequest();
+    	if ($request->isGet()) {
+    		$data = $request->getQuery();
+    		$dir = $data->dir;
+    
+    		$dir = "D:/NewsBin64/download/tmp";
+
+			// Is there actually such a folder/file?
+    		$files = array();
+			if(file_exists($dir)){
+				foreach(scandir($dir) as $f) {
+					if(!$f || $f[0] == '.') {
+						continue; // Ignore hidden files
+					}
+		
+					if(is_dir($dir . '/' . $f)) {
+						// The path is a folder
+						$files[] = array(
+							"name" => $f,
+							"type" => "folder",
+							"path" => $dir . '/' . $f,
+							"items" => scan($dir . '/' . $f) // Recursively get the contents of the folder
+						);
+					} else {
+						// It is a file
+						$files[] = array(
+							"name" => $f,
+							"type" => "file",
+							"path" => $dir . '/' . $f,
+							"size" => filesize($dir . '/' . $f) // Gets the size of this file
+						);
+					}
+				}
+			}
+
+    		$viewmodel = new ViewModel();
+    		$viewmodel->setTerminal(true);
+    
+    		$viewmodel->setVariables(array(
+    				'files' => $files,
+    		));
+    
+    		return $viewmodel;
+    	}
+    }
 }
