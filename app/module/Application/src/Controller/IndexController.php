a0b0a60d3522096059256a6cd16aa523fb26b8fd
Gio-PC\Sergio ARNONE <aqwzsxedc50@live.fr>
2016-12-04 23:00:16 +0100

    correctif / manquant dans top_dir de getVideoDuration

--- app/module/Application/src/Controller/IndexController.php
+++ app/module/Application/src/Controller/IndexController.php
@@ -204,7 +204,7 @@ class IndexController extends AbstractActionController
     	if ($request->isGet()) {
     		$data = $request->getQuery();    
     		$file = $data->file;
-    		$top_dir = "D:/NewsBin64/download";
+    		$top_dir = "D:/NewsBin64/download/";
     
     		if (isset($file)) {
     			$cmd = "ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 " . "\"" . $top_dir . $file . "\"";
